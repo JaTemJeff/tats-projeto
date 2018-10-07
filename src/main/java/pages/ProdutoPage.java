ad1490d2d43fc3601442ba886d4b9809def6bf28
jatemjeff <jff_94@hotmail.com>
2018-10-07 15:39:57 -0300

    Add produto Page + Produto Test

---
+++ src/main/java/pages/ProdutoPage.java
@@ -0,0 +1,124 @@
+package pages;
+
+import core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProdutoPage extends BasePage {
+
+    public ProdutoPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//*[@id=\'sidebar\']/ul/li[3]/a")
+    WebElement link_produtos;
+
+    @FindBy(xpath = "//*[@id=\'content\']/div[2]/div/div/a")
+    WebElement button_adicionar_produto;
+
+    @FindBy(id = "descricao")
+    WebElement input_descricao;
+
+    @FindBy(id = "precoCompra")
+    WebElement input_preco_compra;
+
+    @FindBy(id = "precoVenda")
+    WebElement input_preco_venda;
+
+    @FindBy(id = "unidade")
+    WebElement Select;
+    Select seleciona_unidade = new Select(Select);
+
+    @FindBy(id = "estoque")
+    WebElement input_estoque;
+
+    @FindBy(id = "estoqueMinimo")
+    WebElement input_estoque_minimo;
+
+    @FindBy(xpath = "//*[@id=\'formProduto\']/div[8]/div/div/button")
+    WebElement button_submit;
+
+    @FindBy(xpath = "//*[@id=\'content\']/div[2]/div/div/div[1]")
+    WebElement alert_mensagem;
+
+    @FindBy(xpath = "//*[@id=\'content\']/div[2]/div/div/div[1]/div[2]/table/tbody/tr[1]/td[5]/a[3]")
+    WebElement button_excluir;
+
+    @FindBy(xpath = "//*[@id=\'modal-excluir\']/form/div[3]/button[2]")
+    WebElement button_confirmar_exclusao;
+
+    public ProdutoPage setDescricao(String descricao) {
+        input_descricao.clear();
+        input_descricao.sendKeys(descricao);
+        return this;
+    }
+
+    public ProdutoPage setPrecoCompra(String valor) {
+        input_preco_compra.clear();
+        input_preco_compra.sendKeys(valor);
+        return this;
+    }
+
+    public ProdutoPage setPrecoVenda(String valor) {
+        input_preco_venda.clear();
+        input_preco_venda.sendKeys(valor);
+        return this;
+    }
+
+    public ProdutoPage setUnidade(String opcao) {
+        seleciona_unidade.selectByValue(opcao);
+        return this;
+    }
+
+    public ProdutoPage setEstoque(String quantidade) {
+        input_estoque.clear();
+        input_estoque.sendKeys(quantidade);
+        return this;
+    }
+
+    public ProdutoPage setEstoqueMinimo(String quantidade) {
+        input_estoque_minimo.clear();
+        input_estoque_minimo.sendKeys(quantidade);
+        return this;
+    }
+
+    public void goToProdutoPage() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElements(link_produtos));
+        link_produtos.click();
+    }
+
+    public void clicaAdicionarNovoProduto() {
+        button_adicionar_produto.click();
+    }
+
+    public void submeterAdicionarProduto() {
+        button_submit.click();
+    }
+
+    public String retornaTextoDaMensagem() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElements(alert_mensagem));
+        return alert_mensagem.getText().trim();
+    }
+
+    public void clicaBtnExcluir() {
+        button_excluir.click();
+    }
+
+    public void confirmaExclusao() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElements(button_confirmar_exclusao));
+        button_confirmar_exclusao.click();
+    }
+
+    public void excluir() {
+        goToProdutoPage();
+        clicaBtnExcluir();
+        confirmaExclusao();
+    }
+}
